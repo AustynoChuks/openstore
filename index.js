76723c970f88ee49e3e48c495ea7d5744ba5ffef
AustynoChuks <ewurumaugustine@gmail.com>
2020-06-21 22:30:20 +0100

    Removed nodemodules

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require("express")
+const path = require('path')
 const app = express();
 const PORT = process.env.PORT || 3030;
 
+app.use("/public", express.static(path.join(__dirname, "assets")))
+app.set("views", path.join(__dirname, "views/v1"))
+
+app.set('view engine', 'ejs');
+
 app.get("/", (req, res)=>{
-    res.send("Hello express")
+    res.render("index")
 })
+
 app.listen(PORT, ()=>{
     console.log(`listening on port ${PORT}`)
 })
